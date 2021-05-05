a50ef6f97c861fdbc8e861eb982e545faec6f890
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-05 20:32:48 +0300

    Create complexity.py

---
+++ lab2_2/complexity.py
@@ -0,0 +1,22 @@
+from fft import fastFourierTransformRecursion
+import time
+import sys
+sys.path.append('../')
+from lab1.signalGenerator import createSignal
+from lab2_1.dft import discreteFourierTransform
+
+def getComplexity(stepsCount, harmonics, maxFrequency, type):
+  elapsed = []
+  size = []
+  for i in range(stepsCount):
+    count = int(2 ** (i + 1))
+    size.append(count)
+    signal = createSignal(harmonics, maxFrequency, count)
+    start = time.perf_counter()
+    if (type == "DFT"):
+      discreteFourierTransform(signal, "list")
+    else:
+      fastFourierTransformRecursion(signal)
+    stop = time.perf_counter()
+    elapsed.append(stop - start)
+  return size, elapsed
