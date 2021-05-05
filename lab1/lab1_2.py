c24c00d6d7979bd2df81163cfb3a420a11b48bbf
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-05 20:02:11 +0300

    Create lab1_2.py

---
+++ lab1/lab1_2.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+import time
+import signalGenerator
+import calc
+import complexity
+
+HARMONICS = 12
+MAX_FREQUENCY = 2400
+DISCRETE_CALLS = 1024
+COMPLEXITY_COUNT_LOOPS = 200
+
+signalX = signalGenerator.createSignal(
+  HARMONICS,
+  MAX_FREQUENCY,
+  DISCRETE_CALLS
+)
+signalY = signalGenerator.createSignal(
+  HARMONICS,
+  MAX_FREQUENCY,
+  DISCRETE_CALLS
+)
+
+autocorrelation = calc.correlation(signalX)
+correlation = calc.correlation(signalX, signalY)
+
+elapsedAuto = []
+elapsedCross = []
+for _ in range(COMPLEXITY_COUNT_LOOPS):
+  startAuto = time.perf_counter()
+  calc.correlation(signalX)
+  stopAuto = time.perf_counter()
+  elapsedAuto.append(stopAuto - startAuto)
+
+  startCross = time.perf_counter()
+  calc.correlation(signalX, signalY)
+  stopCross = time.perf_counter()
+  elapsedCross.append(stopCross - startCross)
+avgAuto = calc.mean(elapsedAuto)
+avgCross = calc.mean(elapsedCross)
+file = open("timeTest.txt", "w+")
+file.write('Autocorrelation average time: ' + str(avgAuto) + '\n')
+file.write('Cross-correlation average time: ' + str(avgCross))
+file.close()
+
+harmonics, time = complexity.getCorrealtionComplexity(
+  COMPLEXITY_COUNT_LOOPS,
+  MAX_FREQUENCY,
+  DISCRETE_CALLS
+)
+
+len = int(DISCRETE_CALLS / 2)
+correlationRange = list(range(len))
+
+fig, axs = plt.subplots(2, 2)
+plt.subplots_adjust(left=0.05, bottom=0.1, right=0.97, wspace=0.1)
+fig.suptitle('Lab 1.2')
+
+axs[0, 0].plot(signalX, color='r', linewidth=0.75, label='signal X')
+axs[0, 0].plot(signalY, color='g', linewidth=0.75, label = 'signal Y')
+axs[0, 0].set_title('Generated signals')
+axs[0, 0].set(xlabel='time', ylabel='generated signal')
+axs[0, 0].legend()
+
+axs[0, 1].plot(correlationRange, autocorrelation, color='r', linewidth=0.8)
+axs[0, 1].set_title('Autocorrelation (signal X)')
+axs[0, 1].set(xlabel='t', ylabel='correlation')
+
+axs[1, 0].plot(correlationRange, correlation, linewidth=0.8)
+axs[1, 0].set_title('Cross-correlation')
+axs[1, 0].set(xlabel='t', ylabel='correlation')
+
+axs[1, 1].plot(harmonics, time)
+axs[1, 1].set_title('Correlation calculation complexity')
+axs[1, 1].set(xlabel='n', ylabel='elapsed time (seconds)')
+
+plt.show()
+fig.savefig('graphs/lab1-2.png')
