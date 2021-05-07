b7604586ac4f0a9577bcdd9fc7ecb80924522ad6
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-08 01:10:04 +0300

    Update lab2-1.py

--- lab2_1/lab2-1.py
+++ lab2_1/lab2-1.py
@@ -16,25 +16,12 @@ signal = createSignal(
   MAX_FREQUENCY,
   DISCRETE_CALLS
 )
-spectreDict = discreteFourierTransform(signal, "dict")
-spectreList = discreteFourierTransform(signal, "list")
-lens, elapsedList, avgList = getDFTComplexity(
+spectre = discreteFourierTransform(signal)
+lens, elapsed = getDFTComplexity(
   COMPLEXITY_COUNT_LOOPS,
   HARMONICS,
-  MAX_FREQUENCY,
-  "list"
+  MAX_FREQUENCY
 )
-_, elapsedDict, avgDict = getDFTComplexity(
-  COMPLEXITY_COUNT_LOOPS,
-  HARMONICS,
-  MAX_FREQUENCY,
-  "dict"
-)
-
-file = open("timeTest.txt", "w+")
-file.write('DFT with list() average time: ' + str(avgList) + '\n' +
-  'DFT with dict() average time: ' + str(avgDict))
-file.close()
 
 fig, axs = plt.subplots(3, 1)
 plt.subplots_adjust(left=0.05, top=0.94, bottom=0.05, right=0.97, hspace=0.25)
@@ -50,25 +37,7 @@ axs[1].set(xlabel='p', ylabel='F(p)')
 
 axs[2].plot(lens, elapsedList, color='g')
 axs[2].set_title('DFT complexity')
-axs[2].set(xlabel='signal size', ylabel='time')
-fig.savefig('graphs/lab2-1.png')
+axs[2].set(xlabel='time', ylabel='signal size')
 
-fig, axs = plt.subplots(3, 1)
-plt.subplots_adjust(left=0.05, top=0.94, bottom=0.05, right=0.97, hspace=0.25)
-fig.suptitle('Additional task')
-
-axs[0].plot(spectreList, linewidth=0.8)
-axs[0].set_title('DFT with list()')
-axs[0].set(xlabel='o', ylabel='F(p)')
-
-axs[1].plot(spectreDict.values(), color='r', linewidth=0.8)
-axs[1].set_title('DFT with dict()')
-axs[1].set(xlabel='p', ylabel='F(p)')
-
-axs[2].plot(lens, elapsedList, color='g', label='list()')
-axs[2].plot(lens, elapsedDict, color='b', label='dict()')
-axs[2].set_title('Complexity comparison')
-axs[2].set(xlabel='signal size', ylabel='time')
-axs[2].legend()
-fig.savefig('graphs/additionalTask.png')
 plt.show()
+fig.savefig('graphs/lab2-1.png')
