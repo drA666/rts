ebb749c50295b17481fd598f256c41d15a06e7e5
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-08 00:36:54 +0300

    Update signalGenerator.py

--- lab1/signalGenerator.py
+++ lab1/signalGenerator.py
@@ -11,14 +11,3 @@ def createSignal(harmonics, maxFrequency, calls):
     for t in range(calls):
       sumsArr[t] += amplitude * math.sin(frequency * t + phase)
   return sumsArr
-
-def createSignalAT(harmonics, maxFrequency, calls):
-  sumsArr = [0] * calls
-  frequencyStep = maxFrequency / harmonics
-  for i in range(harmonics):
-    frequency = frequencyStep * (i + 1)
-    for t in range(calls):
-      amplitude = random.random()
-      phase = random.random()
-      sumsArr[t] += amplitude * math.sin(frequency * t + phase)
-  return sumsArr
