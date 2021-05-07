e81b66603e2dbab53bd57f1fe36deb2144f21c0d
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-08 00:59:02 +0300

    Update dft.py

--- lab2_1/dft.py
+++ lab2_1/dft.py
@@ -4,18 +4,14 @@ def fourierCoefficient(pk, N):
   arg = 2 * math.pi * pk / N
   return complex(math.cos(arg), -math.sin(arg))
 
-def discreteFourierTransform(signal, dataType):
+def discreteFourierTransform(signal):
   N = len(signal)
-  spectre = list() if (dataType == "list") else dict()
+  spectre = []
   for p in range(N):
     sum = 0
     for k in range(N):
       x = signal[k]
       w = fourierCoefficient(p*k, N)
       sum += w * x
-    res = abs(sum)
-    if (dataType == "list"):
-      spectre.append(res)
-    else:
-      spectre[p] = abs(res)
-  return spectre
+      spectre.append(abs(sum))
+    return spectre
