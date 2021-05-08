c4d5595e3cc3da432d7b9480a55563b03994f8d8
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-08 23:25:51 +0300

    Update lab2-2.py

--- lab2_2/lab2-2.py
+++ lab2_2/lab2-2.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
-from fft import fastFourierTransformRecursion, fastFourierTransformLoop
+from fft import fastFourierTransform
 from complexity import getComplexity
 import sys
 sys.path.append('../')
 from lab1.signalGenerator import createSignal
+from lab2_1.dft import discreteFourierTransform
 
 HARMONICS = 12
 MAX_FREQUENCY = 2400
@@ -17,8 +18,7 @@ signal = createSignal(
   MAX_FREQUENCY,
   DISCRETE_CALLS
 )
-spectre = toRealNum(fastFourierTransformRecursion(signal))
-spectreAT = toRealNum(fastFourierTransformLoop(signal))
+spectre = toRealNum(fastFourierTransform(signal))
 
 size, elapsedFFT = getComplexity(
   COMPLEXITY_COUNT_LOOPS,
@@ -32,7 +32,7 @@ plt.subplots_adjust(left=0.05, top=0.94, bottom=0.05, right=0.97, hspace=0.25)
 fig.suptitle('Lab 2.2')
 
 axs[0].plot(signal, color='r', linewidth=0.8)
-axs[0].set_title('Generated signal')
+axs[0].set_title('Discrete Fourier transform')
 axs[0].set(xlabel='time', ylabel='signal')
 
 axs[1].plot(spectre, linewidth=0.8)
@@ -43,18 +43,4 @@ axs[2].plot(size, elapsedFFT, color='b')
 axs[2].set_title('FFT Complexity')
 axs[2].set(xlabel='signal size', ylabel='time')
 fig.savefig('graphs/lab2-2.png')
-
-fig, axs = plt.subplots(2, 1)
-plt.subplots_adjust(left=0.05, top=0.94, bottom=0.05, right=0.97, hspace=0.25)
-fig.suptitle('Additional task')
-
-axs[0].plot(signal, color='r', linewidth=0.8)
-axs[0].set_title('Generated signal')
-axs[0].set(xlabel='time', ylabel='signal')
-
-axs[1].plot(spectreAT, color='r', linewidth=0.8)
-axs[1].set_title('FFT')
-axs[1].set(xlabel='p', ylabel='F(p)')
-
-fig.savefig('graphs/additionalTask.png')
 plt.show()
